60e1cc64a43a1556f96b67711750a5fdf36c3806
OSobky <omarelsobky97@gmail.com>
2023-02-25 20:38:09 +0100

    using the prev sum from index and input for output

--- Lists/1480.-Running-Sum-of-1d-Array.py
+++ Lists/1480.-Running-Sum-of-1d-Array.py
@@ -4,8 +4,7 @@ This script solves the problem 1480. Running Sum of 1d Array in LeetCode
 
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        runningSum: List[int] = []
         # get the sum for each element in the running array
-        for i in range(len(nums)):
-            runningSum.append(sum(nums[:i+1])) 
-        return runningSum
+        for i in range(1, len(nums)):
+            nums[i] = nums[i-1] + nums[i]
+        return nums
